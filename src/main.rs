f52521d32e9d10e46c2362c9ab6d12f8b46a7f6f
m1kc (Max Musatov) <m1kc@yandex.ru>
2024-04-13 21:54:19 +0400

    Add reading array header

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,80 @@
 mod kv;
 use kv::Database;
 
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+
+
+fn read_byte(stream: &mut TcpStream) -> Result<u8, std::io::Error> {
+	let mut buffer = [0; 1]; // 1 byte buffer
+	stream.read(&mut buffer)?;
+
+	let ch: String = match buffer[0] {
+		b'\n' => "LF".to_string(),
+		b'\r' => "CR".to_string(),
+		_ => format!("{}", buffer[0] as char),
+	};
+	println!("Read byte: {} (ascii: {})", buffer[0], ch);
+
+	Ok(buffer[0])
+}
+
+
+fn read_byte_and_expect(stream: &mut TcpStream, expected: u8) -> Result<(), std::io::Error> {
+	let b = read_byte(stream)?;
+	if b != expected {
+		return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("Expected byte {}, got {}", expected, b)));
+	}
+	Ok(())
+}
+
+
+fn resp_read_array_header(stream: &mut TcpStream) -> Result<u64, std::io::Error> {
+	read_byte_and_expect(stream, b'*')?;
+	let mut ret: u64 = 0;
+	loop {
+		let b = read_byte(stream)?;
+		if b == b'\r' {
+			break;
+		}
+		assert!(b >= b'0' && b <= b'9', "Invalid character encountered: {}", b as char);
+		ret = ret * 10 + (b - b'0') as u64;
+	}
+	read_byte_and_expect(stream, b'\n')?;
+	return Ok(ret);
+}
+
+
+fn handle_client(mut stream: TcpStream) {
+	// TODO: inline mode isn't implemented yet.
+
+	loop {
+		let num_of_elements = resp_read_array_header(&mut stream).unwrap();
+		println!("Okay, {} elements", num_of_elements);
+	}
+}
+
 
 fn main() {
-	println!("Hello, world!");
+	println!("HUGE SUCCESS");
 
-	let mut storage = Database::new();
-	let key: Vec<u8> = "Carl".as_bytes().to_vec();
-	let val: Vec<u8> = "good".as_bytes().to_vec();
-	storage.insert(key, val);
+	const LISTEN_ADDR: &str = "127.0.0.1";
+	const LISTEN_PORT: u16 = 6379;
+	let l: String = format!("{}:{}", LISTEN_ADDR, LISTEN_PORT);
+	let listener = TcpListener::bind(&l).expect("Failed to bind to address");
+	println!("Server listening on {}", &l);
 
-	println!("got value: {:?}", storage.get("Carl".as_bytes()));
-
-	let k_val = storage.get("Carl".as_bytes());
-	if let Some(value) = k_val {
-		if let Ok(value_str) = std::str::from_utf8(value) {
-			println!("value as string: {}", value_str);
-		} else {
-			println!("Failed to convert value to UTF-8 string");
+	for stream in listener.incoming() {
+		match stream {
+			Ok(stream) => {
+				println!("New client connected");
+				std::thread::spawn(move || {
+					handle_client(stream);
+				});
+			}
+			Err(err) => {
+				println!("Failed to accept client connection: {}", err);
+			}
 		}
-	} else {
-		println!("Key not found");
-	}
-
-	storage.remove("Carl".as_bytes());
-
-	let k_val = storage.get("Carl".as_bytes());
-	if let Some(value) = k_val {
-		if let Ok(value_str) = std::str::from_utf8(value) {
-			println!("value as string: {}", value_str);
-		} else {
-			println!("Failed to convert value to UTF-8 string");
-		}
-	} else {
-		println!("Key not found");
 	}
 }
