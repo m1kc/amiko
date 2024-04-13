29d75d0d375f8fa2449c13200f1ed1c3c524c59a
m1kc (Max Musatov) <m1kc@yandex.ru>
2024-04-13 22:41:56 +0400

    Move protocol stuff to resp.rs

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,10 @@
+mod resp;
+use resp::*;
 mod kv;
 use kv::Database;
 
 use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
-
-
-fn read_byte(stream: &mut TcpStream) -> Result<u8, std::io::Error> {
-	let mut buffer = [0; 1]; // 1 byte buffer
-	stream.read_exact(&mut buffer)?;
-
-	const DEBUG_READ_BYTE: bool = false;
-	if DEBUG_READ_BYTE {
-		let ch: String = match buffer[0] {
-			b'\n' => "LF".to_string(),
-			b'\r' => "CR".to_string(),
-			_ => format!("{}", buffer[0] as char),
-		};
-		println!("Read byte: {} (ascii: {})", buffer[0], ch);
-	}
-
-	Ok(buffer[0])
-}
-
-
-fn read_byte_and_expect(stream: &mut TcpStream, expected: u8) -> Result<(), std::io::Error> {
-	let b = read_byte(stream)?;
-	if b != expected {
-		return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("Expected byte {}, got {}", expected, b)));
-	}
-	Ok(())
-}
-
-
-fn resp_read_array_header(stream: &mut TcpStream) -> Result<u64, std::io::Error> {
-	read_byte_and_expect(stream, b'*')?;
-	let mut ret: u64 = 0;
-	loop {
-		let b = read_byte(stream)?;
-		if b == b'\r' {
-			break;
-		}
-		assert!(b >= b'0' && b <= b'9', "Invalid character encountered: {}", b as char);
-		ret = ret * 10 + (b - b'0') as u64;
-	}
-	read_byte_and_expect(stream, b'\n')?;
-	return Ok(ret);
-}
-
-
-fn resp_expect_bulk_string(stream: &mut TcpStream) -> Result<Vec<u8>, std::io::Error> {
-	read_byte_and_expect(stream, b'$')?;
-	let mut len: u64 = 0;
-	loop {
-		let b = read_byte(stream)?;
-		if b == b'\r' {
-			break;
-		}
-		assert!(b >= b'0' && b <= b'9', "Invalid character encountered: {}", b as char);
-		len = len * 10 + (b - b'0') as u64;
-	}
-	read_byte_and_expect(stream, b'\n')?;
-	let mut buf = vec![0; len as usize];
-	stream.read_exact(&mut buf)?;
-	read_byte_and_expect(stream, b'\r')?;
-	read_byte_and_expect(stream, b'\n')?;
-
-	println!("Read bulk string: {:?}", buf);
-	let buf_as_string = String::from_utf8_lossy(&buf);
-	println!("Bulk string as UTF-8 string: {}", buf_as_string);
-
-	return Ok(buf);
-}
+use std::io::*;
 
 
 fn handle_client(mut stream: TcpStream) {
