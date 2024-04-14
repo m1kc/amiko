371056b6d2de8fb737ca986f88a1b7ac924e9cec
m1kc (Max Musatov) <m1kc@yandex.ru>
2024-04-14 18:11:23 +0400

    Experiment with benchmark params

--- benches/kvbench.rs
+++ benches/kvbench.rs
@@ -3,12 +3,16 @@ use amiko::kv::Database;
 #[macro_use]
 extern crate criterion;
 use criterion::Criterion;
+use smallvec::SmallVec;
 
 
 fn bench_method1(c: &mut Criterion) {
 	let mut db = Database::default();
+	let mut group = c.benchmark_group("group");
+	group.warm_up_time(std::time::Duration::from_secs(1));
+	group.measurement_time(std::time::Duration::from_secs(5));
 
-	c.bench_function("key insert", |b| {
+	group.bench_function("key insert", |b| {
 		b.iter(|| {
 			let n = rand::random::<u32>();
 			let key = format!("key{}", n).into_bytes();
@@ -19,6 +23,8 @@ fn bench_method1(c: &mut Criterion) {
 			assert_eq!(db.get(&key), Some(value));
 		});
 	});
+
+	group.finish();
 }
 
 criterion_group!(benches, bench_method1);
